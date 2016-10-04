4b8eda77985aeaa5c0a221819efeaea94724eae0
BhuvaneshR <rbh14301.ee@rmkec.ac.in>
2016-10-04 13:47:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,3 +9,8 @@ function moveRight()
 img.onclick=function(){
 var interval=setInterval(moveRight,50);
 };
+var photo=document.getElementById('pazhamalai');
+photo.onclick=function()
+{
+    var inter=setInterval(moveRight,100);
+};
