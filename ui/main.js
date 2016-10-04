4af6c2362f30a41f7c48f5d0e1e09b17ce24d857
BhuvaneshR <rbh14301.ee@rmkec.ac.in>
2016-10-04 13:24:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
 console.log('Loaded!');
 var img=document.getElementById('logo');
+var marginLeft=0;
+function moveRight()
+{
+ marginLeft=marginLeft+5;  
+ img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
-img.style.marginLeft='500px';
+var interval=setInterval(moveRight,50);
 };
