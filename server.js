419657843c1fb0e01807e43c93f2d6af43c6b74a
BhuvaneshR <rbh14301.ee@rmkec.ac.in>
2016-10-31 10:34:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.get('/profile.html', function (req, res)
 var counter=0;
 app.get('/counter', function(req,res)
 {
-    counter = counter+1;
-    res.send(counter);
+    counter = counter + 1;
+    res.send(counter.toString());
 }
 );
 app.get('/ui/style.css', function (req, res) {
