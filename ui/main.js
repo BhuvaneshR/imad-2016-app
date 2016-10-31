599d115934d23cd60b3e540ceb75aa5c4111dc84
BhuvaneshR <rbh14301.ee@rmkec.ac.in>
2016-10-31 10:48:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,8 @@
-console.log('Loaded!');
-var img=document.getElementById('logo');
-var marginLeft=0;
-function moveRight()
-{
- marginLeft=marginLeft+5;  
- img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-var interval=setInterval(moveRight,50);
-};
-var img=document.getElementById('pazhamalai');
-img.onclick=function()
-{
-    var inter=setInterval(moveRight,100);
-};
 
 
 
-var button=document.getElementById('_btn');
+
+var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function()
 {
