4876f1507a81d8f5a96cc40e764849b9494c0573
BhuvaneshR <rbh14301.ee@rmkec.ac.in>
2016-10-31 11:52:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,35 @@ button.onclick=function()
     request.open("GET","http://bhuvaneshr.imad.hasura-app.io/counter",true);
     request.send(null);
 };
+
+
+
+
+var nameinput=document.getElementById('name');
+var name = nameInput.value; 
+var submit=document.getElementById('submitbtn');
+submit.onclick=function()
+{
+  
+  
+    var request=new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState=== XMLHttpRequest.DONE)
+        if(request.status===200){
+            var names=['name1','name2','name3'];
+            var list='';
+            for(var i=0;i<names.length;i++)
+            {
+                list += '<li>' + name[i] + '<li>';
+            }
+            var ul=document.getElementById('namelist');
+            ul.innerHTML=list;
+        }
+        
+        
+    };
+   
+    request.open("GET","http://bhuvaneshr.imad.hasura-app.io/submit-name?name="+name,true);
+    request.send(null);
+    
+};
