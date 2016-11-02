4b19f0bf8729cdbbb82a48056e6322b8fe87f490
BhuvaneshR <rbh14301.ee@rmkec.ac.in>
2016-11-02 15:40:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,87 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var articles={
+   'articleone':{
+    title:'Article One',
+    content:`
+<div>
+       <a href='/'>Home</a><br/>
+       <a href='/profile.html'>profile</a><br/>
+       <a href='/articletwo'>Article two</a><br/>
+       <a href='/articlethree'>Article three</a>
+</div>
+<div>
+       <p>October 30</p>
+</div>
+<div>
+       <p>This is my first article</p>
+</div>
+<div>
+       <a href='https://www.google.co.in/imgres?imgurl=https%3A%2F%2Fpixabay.com%2Fstatic%2Fuploads%2Fphoto%2F2015%2F04%2F04%2F19%2F13%2Fone-706897_640.jpg&imgrefurl=https%3A%2F%2Fpixabay.com%2Fen%2Fone-1-number-design-collection-706897%2F&docid=Dxk_1n6Yd4M41M&tbnid=H9VLy5g15akogM%3A&w=640&h=640&hl=en&bih=659&biw=1366&ved=0ahUKEwiF5ruwy4LQAhXIK48KHYslB3AQMwg-KAwwDA&iact=mrc&uact=8' target='_blank'><img src='/ui/one.png' alt='article one' class='img-medium'/></a>
+</div>`
+},
+   'articletwo':{
+       title:'Article Two',
+       content:`     <div>
+<a href='/'>Home</a><br/>
+<a href='/profile.html'>profile</a><br/>
+<a href='/articleone'>Article one</a><br/>
+<a href='/articlethree'>Article three</a>
+</div>
+<div>
+<p>October 30</p>
+</div>
+<div>
+<p>This is my second article</p>
+</div>
+<div>
+<a href='https://www.google.co.in/imgres?imgurl=http%3A%2F%2Fwww.atvtoday.co.uk%2Fwp-content%2Fuploads%2F2014%2F03%2Fbbc-two.jpg&imgrefurl=http%3A%2F%2Fwww.atvtoday.co.uk%2F7483-bbc%2F&docid=oR48HW34-OwJFM&tbnid=03ZQgjlRbkXV4M%3A&w=874&h=339&hl=en&bih=659&biw=1366&ved=0ahUKEwizmrifzILQAhWIu48KHe9dAYsQMwhbKB4wHg&iact=mrc&uact=8' target='_blank'><img src='/ui/two.jpeg' alt='article two' class='img-medium'/></a>
+</div>`
+   },
+   'articlethree':{
+       title:'Article Three',
+       content:` <div>
+<a href='/'>Home</a><br/>
+<a href='/profile.html'>profile</a><br/>
+<a href='/articleone'>Article one</a><br/>
+<a href='/articletwo'>Article two</a><br/>
+</div>
+<div>
+<p>October 30</p>
+</div>
+<div>
+<p>This is my third article</p>
+</div>
+<div>
+<a href='https://www.google.co.in/imgres?imgurl=http%3A%2F%2Fwww.freefoto.com%2Fimages%2F2000%2F03%2F2000_03_52---Number-Three_web.jpg&imgrefurl=http%3A%2F%2Fwww.freefoto.com%2Fpreview%2F2000-03-52%2FNumber-Three&docid=snq6DZOc3ApG4M&tbnid=fDyLAxJU8PMUmM%3A&w=400&h=600&hl=en&bih=659&biw=1366&ved=0ahUKEwiZxpCZzYLQAhWKt48KHazwDa44ZBAzCDgoNjA2&iact=mrc&uact=8' target='_blank'><img src='/ui/three.jpeg' alt='third article' class='img-medium'/></a>
+</div>`
+   }
+};
+function createtemplate(data){
+    var title=data.title;
+    var content=data.content;
+var htmltemplate=`<!DOCTYPE html>
+<html>
+<head>
+    <link href='/ui/style.css' rel='stylesheet'/>
+    <title>${title}</title>
+</head>
+<body>
+    ${content}
+</body>
+</html>`
+;
+return htmltemplate;
+}
+
+app.get('/:articlename',function(req,res){
+    var articlename=req.params.articlename;
+     res.send(createtemplate(articles[articlename]));
+});
+
+
 app.get('/profile.html', function (req, res)
 {
   res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
